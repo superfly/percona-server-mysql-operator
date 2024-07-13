83e719dd3653d02bc54ae6aedb4db4ee8a3090f2
Joshua Sierles <joshua@fly.io>
2024-07-13 12:13:25 +0200

    Bump fks image for operator

--- fks/build.sh
+++ fks/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-IMAGE=flyio/percona-server-mysql-operator:fks-23
+IMAGE=flyio/percona-server-mysql-operator:fks-24
 
 docker tag perconalab/percona-server-mysql-operator:js-fks $IMAGE
 docker push $IMAGE
