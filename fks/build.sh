a98f036ba16e93f7523f072ed381a150e2ff8533
Joshua Sierles <joshua@fly.io>
2024-07-15 17:42:41 +0200

    more cleanup

--- fks/build.sh
+++ fks/build.sh
@@ -4,3 +4,5 @@ IMAGE=flyio/percona-server-mysql-operator:js-fks
 
 docker tag perconalab/percona-server-mysql-operator:js-fks $IMAGE
 docker push $IMAGE
+
+cd mysql && ./build.sh && cd ../haproxy && ./build.sh && cd ../orchestrator && ./build.sh
